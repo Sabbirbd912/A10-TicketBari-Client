418f995cc02afd5ea391145a877dd47093fd851c
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-07-20 17:14:50 +0600

    contact page designed

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import AboutDiscover from '@/components/AboutDiscover';
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <AboutDiscover />
+        </div>
+    );
+};
+
+export default AboutPage;
